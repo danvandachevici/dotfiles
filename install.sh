5df8b8faa9e3fcccf7e468d8cd66c94f504db62e
Dan VANDACHEVICI <dan@vandachevici.ro>
2022-06-05 20:12:16 +0200

    Added todo

--- install.sh
+++ install.sh
@@ -3,19 +3,20 @@
 #show commands as they happen
 set -o xtrace
 
+CURPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # VIM
 VIMFOLDER=$HOME/.vim
 [ ! -d ~/.vim ] && mkdir ~/.vim
-[ ! -f ~/.vimrc ] && ln -s $PWD/vimrc $VIMFOLDER/.vimrc
-[ ! -f ~/.vim/plugins.vim ] && ln -s $PWD/plugins.vim $VIMFOLDER/plugins.vim
-[ ! -f ~/.vim/statusline.vim ] && ln -s $PWD/statusline.vim $VIMFOLDER/statusline.vim
+[ ! -f ~/.vimrc ] && ln -s $CURPATH/vimrc $VIMFOLDER/.vimrc
+[ ! -f ~/.vim/plugins.vim ] && ln -s $CURPATH/plugins.vim $VIMFOLDER/plugins.vim
+[ ! -f ~/.vim/statusline.vim ] && ln -s $CURPATH/statusline.vim $VIMFOLDER/statusline.vim
 
 # BASH
-[ ! -f ~/.bashrc ] && ln -s $PWD/bashrc $HOME/.bashrc && source $HOME/.bashrc
+[ ! -f ~/.bashrc ] && ln -s $CURPATH/bashrc $HOME/.bashrc && source $HOME/.bashrc
 
 # ZSH
-[ ! -f ~/.zshrc ] && ln -s $PWD/bashrc $HOME/.zshrc && source $HOME/.zshrc
+[ ! -f ~/.zshrc ] && ln -s $CURPATH/zshrc $HOME/.zshrc && source $HOME/.zshrc
 
 # TMUX
-[ ! -f ~/.tmux.conf ] && ln -s $PWD/tmux.conf $HOME/.tmux.conf
+[ ! -f ~/.tmux.conf ] && ln -s $CURPATH/tmux.conf $HOME/.tmux.conf
